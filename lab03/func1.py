af32710a49ed2f2fe429c94f2b14624a43891c5e
1he0den <r_sabyrov@kbtu.kz>
2025-02-03 18:21:13 +0600

    Func1.py

--- lab03/func1.py
+++ lab03/func1.py
@@ -51,7 +51,14 @@ PrimeNums()
 
 # 5th exercise
 
-# не понял пока
+import itertools
+def permutation_printer(word):
+    word_permts=itertools.permutations(word)
+    for perm in word_permts:
+        print(''.join(perm))
+
+word=input("Please enter a word:")
+permutation_printer(word)
 
 
 # 6th exercise
